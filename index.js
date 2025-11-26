71033d5ef6f723614ae8b7c1717430180069d3bb
Coder. Naiem <126259025+Naiem548@users.noreply.github.com>
2025-11-26 17:57:35 +0600

    index-update

--- index.js
+++ index.js
@@ -56,5 +56,5 @@ app.get("/", (req, res) => {
 
 // Start server
 app.listen(port, () => {
-  console.log(`Server running at https://bmw-drivex-server.vercel.app:${port}`);
+  console.log(`✅ Server running on port ${port}`);
 });
